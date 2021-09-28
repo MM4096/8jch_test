d98494465a06b2308e9ac108acfca1179bf662aa
enoch wu <enoch.wu@kingsway.school.nz>
2021-09-28 14:54:34 +1300

    cookie multiple pages

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var allowCookies = 1;
+function accepted() {
+    allowCookies = 1;
+    document.getElementById("cookieFooter").style.visibility = "hidden";
+    setCookie("cookiesEnabled", allowCookies, 365);
+}
+function notAccepted() {
+    allowCookies = 0;
+    document.getElementById("cookieFooter").style.visibility = "hidden";
+    setCookie("cookiesEnabled", allowCookies, 365);
+}
+function setCookie(cookieName, cookieValue, cookieValidDays) {
+    if (allowCookies == 1) {
+        const day = new Date();
+        day.setTime(day.getTime() + (cookieValidDays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + day.toUTCString();
+        document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+    }
+
+}
+function getCookie(cookieName) {
+    let name = cookieName + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+    document.getElementById("returnCookieValue").innerHTML = c;
+}
+function checkCookie() {
+    allowCookies = getCookie("cookiesEnabled");
+}
