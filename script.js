6389c40d52f470d632dd40710790891a3a2a3a68
enoch wu <enoch.wu@kingsway.school.nz>
2021-09-30 16:08:10 +1300

    Merge to main site

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ function calculateCost(itemNumber) {
         0.5, // Chocolate favorates
         //cost of item 4
         0.5, //chupa chups
+        //cost of item 5
+        0.5, //Sour candy
     ]
     itemCost = costs[itemNumber];
     numberItems = +document.getElementById("qty").value;
@@ -74,3 +76,36 @@ function updateCookie(cookieName) {
     setCookie(cookieName, numberItems, 365);
 
 }
+
+
+
+//Coundown for sales thanks to w3Schools
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "days " + hours + "hours "
+        + minutes + "minutes " + seconds + "sseconds ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
