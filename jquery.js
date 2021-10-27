d52efc5cce9b062fa30e182c09b7c0e972941f29
enoch wu <enoch.wu@kingsway.school.nz>
2021-10-27 14:27:09 +1300

    Started search bar. still got problems

--- jquery.js
+++ jquery.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    
+    const items = ["","chocolate favorates", "chupa chups", "licorice allsorts", "lolly scramble", "mystery box", "sour candy"];
     $("#cost").click(function () {
         var txt = $("#cost").text();
         if (added == 1) {
@@ -9,4 +11,29 @@ $(document).ready(function () {
         setTimeout(function () { $("#cost").text(txt);}, 1000);
         
     })
-})
+    //search function
+    //Made by me!
+
+    $("#search").on("keypress", function (e) {
+        if (e.which == 13) {
+            console.log("started!")
+            var searchVal = $("#search").val();
+            var searchValue = searchVal.toLowerCase();
+            console.log(searchValue);
+            for (let i = 1; i < 7; i++) {
+                console.log("started search")
+                console.log(i);
+                if (items[i].includes(searchValue)) {
+                    console.log("true")
+                    $("#" + i).show
+                } else {
+                    console.log("false")
+                    $("#" + i).hide
+                }
+            }
+                
+
+                                
+            }
+        })
+    })
