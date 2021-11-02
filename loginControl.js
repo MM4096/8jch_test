3edf74a518142217317d018e0b3514c273d7ff4b
enoch wu <enoch.wu@kingsway.school.nz>
2021-11-02 14:25:41 +1300

    made admin avalible to all

--- loginControl.js
+++ loginControl.js
@@ -13,6 +13,8 @@ function login() {
     var password = document.getElementById("password").value;
     console.log(username);
     console.log(password);
+    localStorage.setItem("username", username)
+    localStorage.setItem("password", password)
     for (var i = 0; i < usernames.length; i++) {
         if ((usernames[i] === username) && (passwords[i] === password)) {
             console.log("Logged In!")
